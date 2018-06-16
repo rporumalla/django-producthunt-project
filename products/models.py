94cbe3e14dec8d5956a94210f2bdff588cc2697f
Rajiv <rporumalla@apple.com>
2018-06-16 11:36:47 -0700

    polish to products page

--- products/models.py
+++ products/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Product class
+class Product(models.Model):
+    # title
+    title = models.CharField(max_length=255)
+
+    # url
+    url = models.TextField()
+
+    # pub_date
+    pub_date = models.DateTimeField()
+
+    # votes_total
+    votes_total = models.IntegerField(default=1)
+
+    # image
+    image = models.ImageField(upload_to='images/')
+
+    # icon
+    icon = models.ImageField(upload_to='images/')
+
+    # body
+    body = models.TextField()
+
+    # hunter
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100] + "..."
+
+    # pub_date_pretty
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
